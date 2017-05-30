9ef82a55804a55411b53127d6d7f3e1e5da311ed
etaylor113 <etaylor113@gmail.com>
2017-05-30 11:50:29 -0500

    Added weather class

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Stand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu Inititate = new MainMenu();
+            Inititate.StartGame();
+       
+
+        }
+    }
+}
